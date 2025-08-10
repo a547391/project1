8cc77998fff9b45058346520147f3bd330d18183
joyce <a547395@gmai.com>
2025-08-10 17:39:25 +0800

    將footer分離成php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div id="last-data" class="row text-center">
+        <div class="col-md-12">
+            <h6>騎樂騎單車有限公司 40858台中市南屯區騎行路168號　電話：04-27894561　客服專線：0800-889-556</h6>
+            <h6>本公司通過 ISO 9001 / 14001 品質與環境雙重驗證，營業登記字號：24738102</h6>
+            <h6>版權所有 copyright © 2025 RideJoy Bike Co., Ltd. All Rights Reserved.</h6>
+        </div>
+    </div>
+</div>
